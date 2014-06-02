2a46a16877c93adb4c8e504bb9e21feb6cef3ed6
Dmytro_Dombrovskyi_home_PC <dombrovskiy11@gmail.com>
2014-06-02 22:54:16 +0300

    last changes 2.06.

--- modifying_sequence_operations/modifying_sequence_operatons.cpp
+++ modifying_sequence_operations/modifying_sequence_operatons.cpp
@@ -16,7 +16,7 @@ template<typename T>
 void print(T & widget) {cout << widget << " ";}
 int main()
 {
-    //#if 0
+    #if 0
     // copy
     cout << "1. copy algorithm: \n";
     vector<string> names1 { "Dmytro", "Hanna", "Andrij" };
@@ -83,7 +83,6 @@ int main()
     cout << "Arr_1 after using copy_backward algorithm: " << endl;
     for(auto x : arr_1) cout << x << " ";
     cout << endl;
-    //#endif
 
     // move
     cout << "\n5. move algorithm: \n" << endl;
@@ -132,8 +131,96 @@ int main()
     for(auto x: arr_2) cout << " [" << x << "]";
     cout << endl;
 
+    // swap
+    cout << "\n7. swap algorithm: \n" << endl;
 
+    int a = 10, b = 20;
+    cout << "a: " << a << " , ";
+    cout << "b: " << b << " . ";
+    cout << endl;
 
+    swap(a, b);
+    cout << "After function swap called: \n";
+    cout << "a: " << a << " , ";
+    cout << "b: " << b << " .\n";
+    cout << endl;
+
+    vector<int> Alpha(2, a), Betta(3, b);
+    cout << "Alpha contains: " << Alpha.size() << " elements: ";
+    for(auto x : Alpha) cout << " [" << x << "]";
+    cout << endl;
+    cout << "Betta contains: " << Betta.size() << " elements: ";
+    for(auto x : Betta) cout << " [" << x << "]";
+    cout << endl;
+
+    swap(Alpha, Betta);
+    cout << "After function swap called: \n";
+    cout << "Alpha contains: " << Alpha.size() << " elements: ";
+    for(auto x : Alpha) cout << " [" << x << "]";
+    cout << endl;
+    cout << "Betta contains: " << Betta.size() << " elements: ";
+    for(auto x : Betta) cout << " [" << x << "]";
+    cout << endl;
+
+    // swap_ranges
+    cout << "\n8. swap_ranges algorithm: \n" << endl;
+    vector<int> Sr_1(5, 10);
+    vector<int> Sr_2(3, 20);
+    cout << "Before function swap_ranges: \n";
+    cout << "Sr_1 contains " << Sr_1.size() << " elements: ";
+    for(auto x : Sr_1) cout << " [" << x << "]";
+    cout << endl;
+    cout << "Sr_2 contains " << Sr_2.size() << " elements: ";
+    for(auto x : Sr_2) cout << " [" << x << "]";
+    cout << endl;
+
+    swap_ranges(Sr_1.begin(), Sr_1.end(), Sr_2.begin());
+
+    cout << "After function swap_ranges: \n";
+    cout << "Sr_1 contains " << Sr_1.size() << " elements: ";
+    for(auto x : Sr_1) cout << " [" << x << "]";
+    cout << endl;
+    cout << "Sr_2 contains " << Sr_2.size() << " elements: ";
+    for(auto x : Sr_2) cout << " [" << x << "]";
+    cout << endl;
+
+    // iter_swap
+    cout << "\n9. iter_swap algorithm: \n" << endl;
+    deque<int> itSw_1 {1, 2, 3, 4, 5};
+    deque<int> itSw_2 {6, 7, 8, 9, 0};
+    cout << "before function iter_swap: \n";
+    for(auto x : itSw_1) cout << " [" << x << "]";
+    cout << endl;
+    for(auto x : itSw_2) cout << " [" << x << "]";
+    cout << endl;
+
+    cout << "after function iter_swap: \n";
+    iter_swap(itSw_2.begin() + 1, itSw_1.begin() + 3);
+    for(auto x : itSw_1) cout << " [" << x << "]";
+    cout << endl;
+    for(auto x : itSw_2) cout << " [" << x << "]";
+    cout << endl;
+
+    #endif
+    // transform
+    cout << "\n10. transform algorithm: \n" << endl;
+
+    vector<int> tr_1;
+    vector<int> tr_2;
+    for(int i = 0; i < 10; ++i) tr_1.push_back((i* 10));
+    tr_2.resize(tr_1.size());
+
+    cout << "before transform: \n";
+    cout << "tr_1 contains " << tr_1.size() << " elementrs: ";
+    for(auto x : tr_1) cout << " [" << x << "]";
+    cout << endl;
+
+   // transform(tr_1.begin(), tr_1.end(), tr_2.begin(),
+    //          tr_2.end(), plus<int>());
+    cout << "After transform: \n";
+    cout << "tr_1 contains " << tr_1.size() << " elementrs: ";
+    for(auto x : tr_1) cout << " [" << x << "]";
+    cout << endl;
 
 
 
