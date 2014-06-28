6f77ab23073a7e4417a877ba1f990a911128857b
Dima_comp_by_Anna <dombrovskiy11@gmail.com>
2014-06-28 09:55:33 +0300

    add includes, set_union algorithms

--- merge/merge.cpp
+++ merge/merge.cpp
@@ -13,6 +13,13 @@ using std::string;
 
 typedef vector<int> vec_int;
 
+template<typename T>
+class compare
+{
+    public:
+    bool operator () (T a, T b) { return a < b;}
+};
+
 int main()
 {
 #if 0
@@ -47,7 +54,7 @@ int main()
     for(auto x : merge_arr_2) cout << x << " "; cout << endl;
     cout << "Data in array 3: ";
     for(auto x : merge_arr_result) cout << x << " "; cout << endl;
-#endif
+
     // #2. inplace_merge
     cout << "#2. inplace_merge: " << endl;
     vec_int data1, data2, data3;
@@ -76,9 +83,60 @@ int main()
     cout << "Data in array 3: ";
     for(auto x : data3) cout << x << " "; cout << endl;
     cout << endl;
-
+#endif
     // #3. includes
     cout << "#3. includes: " << endl;
+    vec_int data_incl_1;
+    vec_int data_incl_2;
+
+    for(int i = 0; i < 10; ++i) data_incl_1.push_back(i);
+    for(int i = 5; i < 10; ++i) data_incl_2.push_back(i);
+
+    cout << "Data 1: ";
+    for(auto x : data_incl_1) cout << x << " "; cout << endl;
+    cout << "Data 2: ";
+    for(auto x : data_incl_2) cout << x << " "; cout << endl;
+
+    cout << "Data2 in Data1 are ";
+    compare<int> comparator_1;
+    if(std::includes(data_incl_1.begin(), data_incl_1.end(),
+       data_incl_2.begin(), data_incl_2.end(), comparator_1))
+       {
+           cout << "includ!" << endl;
+       }
+       else
+        cout << "not includ!" << endl;
+    cout << endl;
+
+    // #4. set_union
+    cout << "#4. set_union: " << endl;
+    data_incl_1.clear(); data_incl_2.clear();
+    for(int i = 0; i < 10; ++i)
+    {
+        data_incl_1.push_back(i);
+        data_incl_2.push_back(i * 2);
+    }
+    cout << "data in array 1: ";
+    for(auto x : data_incl_1) cout << x << ' '; cout << endl;
+    cout << "data in array 2: ";
+    for(auto x : data_incl_2) cout << x << ' '; cout << endl;
+
+    vec_int arr_set_union(data_incl_1.size() + data_incl_2.size());
+    vec_int::iterator arr_itr;
+    arr_itr = std::set_union(data_incl_1.begin(), data_incl_1.end(),
+                   data_incl_2.begin(), data_incl_2.end(),
+                   arr_set_union.begin());
+    arr_set_union.resize(arr_itr - arr_set_union.begin());
+
+    cout << "Data in union array: ";
+    for(auto x : arr_set_union) cout << x << ' '; cout << endl;
+    cout << endl;
+
+
+    // #5. set_intersection
+    cout << "#5. set_intersection: " << endl;
+
+
 
 
 
