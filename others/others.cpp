cb4c813a0f83661ebc634cc4c9744acbf82eca3c
Dima_home_debian <dombrovskiy11@gmail.com>
2014-06-30 09:02:37 +0300

    start work with others algorithms, created new file: others.cpp

---
+++ others/others.cpp
@@ -0,0 +1,19 @@
+// others algorithms
+// Dmitrij Dombrovskyi // 30.06.2014
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main()
+{
+
+    return 0;
+}
